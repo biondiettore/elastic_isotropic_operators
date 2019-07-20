c76312d2ce94a6a332d87a38c40b3516e8530854
Ettore Biondi <ettore88@stanford.edu>
2019-07-19 17:06:34 -0700

    Changed SUBMAX to 100 to allow higher subsampling

--- elastic_iso_lib/elastic_iso_float/prop/include/varDeclare.h
+++ elastic_iso_lib/elastic_iso_float/prop/include/varDeclare.h
@@ -9,7 +9,7 @@
 #define COEFF_SIZE 4 // Laplacian coefficient array for 10th order
 #define PI_CUDA M_PI // Import the number "Pi" from the math library
 #define PAD_MAX 500 // Maximum number of points for padding (on one side)
-#define SUB_MAX 70 // Maximum subsampling value
+#define SUB_MAX 100 // Maximum subsampling value
 
 #define min2(v1,v2) (((v1)<(v2))?(v1):(v2)) /* Minimum function */
 #define max2(v1,v2) (((v1)>(v2))?(v1):(v2)) /* Minimum function */
